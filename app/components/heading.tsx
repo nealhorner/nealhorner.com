085f6bce98cf4e2cc702e9caa6f270dc4e56839e
Neal Horner <thenealhorner@gmail.com>
2025-11-11 12:23:23 -0800

    feat: Add Heading component to display the name prominently in the header

---
+++ app/components/heading.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Heading() {
+  return (
+    <header className="py-4 m-0">
+      <h1 className="text-5xl xs:text-xl text-center text-zinc-900 dark:text-zinc-100">
+        Neal Horner
+      </h1>
+    </header>
+  );
+}
